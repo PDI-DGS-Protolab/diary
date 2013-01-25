061d7f7e09f87864d9c6b6691542c1568cb9837b
Jimmy coll <acido69@gmail.com>
2013-01-25 14:29:15 +0100

    fixed #2

--- script/controller/add.js
+++ script/controller/add.js
@@ -1,4 +1,37 @@
 angular.module('app').controller('add',['$rootScope', '$scope', 'webdb',function(rootScope, scope, webdb){
+  var data = {
+    note:'example',
+    position:''
+  };
+  var init = function(){
+    scope.data = data;
+    scope.addNote = addNote;
+    scope.addPosition = addPosition;
+    scope.setLocation = setLocation;
+  };
+  //Public
+  var addNote = function(){
+    var day = rootScope.currentDay;
+    var note = scope.data.note;
+    if(day){
+      webdb.addNote(day, note, _onAdd, _onError);
+    }
+  };
+  var addPosition = function(){
+    var day = rootScope.currentDay;
+    var position = scope.data.position;
+    webdb.addPosition(day, position, _onAdd, _onError);
+  };
+  var setLocation = function(){
+    _getLocation(function(position){
+      scope.$apply(function() {
+          scope.data.position = _positionToString(position);
+      });
+    }, function(e){
+      //console.log(e);
+    });
+  };
+  //PRIVATE
   var _getLocation = function(success, error){
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(success, error);
@@ -16,36 +49,8 @@ angular.module('app').controller('add',['$rootScope', '$scope', 'webdb',function
   var _onError = function(e){
     alert(e);
   };
-  scope.data = {
-    note:'example',
-    position:''
-  };
-  scope.addNote = function(){
-    var day = rootScope.currentDay;
-    var note = scope.data.note;
-    if(day){
-      webdb.addNote(day, note, _onAdd, _onError);
-    }else{
-      alert('Day no existe');
-      alert(day);
-    }
 
-
-  };
-  scope.addPosition = function(){
-    var day = rootScope.currentDay;
-    var position = scope.data.position;
-    webdb.addPosition(day, position, _onAdd, _onError);
-  };
-  scope.setLocation = function(){
-    _getLocation(function(position){
-      scope.$apply(function() {
-          scope.data.position = _positionToString(position);
-      });
-    }, function(e){
-      //console.log(e);
-    });
-  };
+  init();
 
 
 
